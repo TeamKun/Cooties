77d2df53a7d9fb5221831820cc86792e8501a212
POne0301 <onexttt@gmail.com>
2021-06-14 23:51:56 +0900

    first commit

---
+++ src/main/java/net/kunmc/lab/cooties/Cooties.java
@@ -0,0 +1,17 @@
+package net.kunmc.lab.cooties;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Cooties extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
