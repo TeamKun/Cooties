1d874b2d7dca7a88bf81a2d23a6f93c4dc47bc78
POne0301 <onexttt@gmail.com>
2021-06-18 02:21:32 +0900

    add runnable

---
+++ src/main/java/net/kunmc/lab/cooties/task/Task.java
@@ -0,0 +1,30 @@
+package net.kunmc.lab.cooties.task;
+
+import net.kunmc.lab.cooties.cooties.CootiesContext;
+import net.kunmc.lab.cooties.game.GameManager;
+import net.kunmc.lab.cooties.player.PlayerState;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class Task extends BukkitRunnable {
+    private JavaPlugin plugin;
+
+    public Task(JavaPlugin plugin){
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void run() {
+        if (GameManager.runningMode == GameManager.GameMode.MODE_START)
+            return;
+
+        // Update処理を実行
+        for (PlayerState ps: GameManager.playerStateList){
+            Player p = ps.getPlayer();
+            for (CootiesContext cc: ps.getCooties()){
+                cc.runCootiesProcess(p);
+            }
+        }
+    }
+}
