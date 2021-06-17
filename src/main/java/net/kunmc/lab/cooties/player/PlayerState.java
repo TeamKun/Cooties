9622aab6b57767831b42ddcda3060e783adab845
POne0301 <onexttt@gmail.com>
2021-06-18 02:20:53 +0900

    remove(move to PlayerProcess) swap, init cooties process

--- src/main/java/net/kunmc/lab/cooties/player/PlayerState.java
+++ src/main/java/net/kunmc/lab/cooties/player/PlayerState.java
@@ -1,46 +1,35 @@
 package net.kunmc.lab.cooties.player;
 
-import net.kunmc.lab.cooties.cooties.CootiesInterface;
-import net.kunmc.lab.cooties.cooties.players.PlayerCootiesFactory;
-import org.bukkit.Bukkit;
+import net.kunmc.lab.cooties.cooties.CootiesContext;
 import org.bukkit.entity.Player;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
-public class PlayerState {
-    static Map<String, List<CootiesInterface>> cooties;
+public class PlayerState implements Cloneable{
+    Player player;
+    List<CootiesContext> cooties;
 
-    public static void initPlayerState(){
-        cooties = Bukkit.getOnlinePlayers().stream().collect(
-                Collectors.toMap(e -> e.getName(),
-                                 e -> PlayerCootiesFactory.createCooties(e.getName())));
+    public PlayerState(Player player, List<CootiesContext> cooties){
+        this.player = player;
+        this.cooties = cooties;
     }
 
-    public static void swapPlayerCooties(Player touchedPlayer, Player touchPlayer){
-        /* - 菌交換の流れ
-         *   - 触れ合ったPlayer同士の菌を交換
-         *     - 交換したのが菌持ちの特定プレイヤーであれば、自身の菌を追加しておく
-         */
-        String touchedPlayerName = touchedPlayer.getName();
-        String touchPlayerName = touchedPlayer.getName();
-
-        List tmpTouchedPlayerCooties = cooties.get(touchedPlayerName);
-        List tmpTouchPlayerCooties = cooties.get(touchPlayerName);
-
-        cooties.get(touchedPlayerName).clear();
-        cooties.get(touchedPlayerName).addAll(tmpTouchPlayerCooties);
-        removeCooties(touchedPlayer);
-
-        cooties.get(touchPlayer.getName()).clear();
-        cooties.get(touchPlayer.getName()).addAll(tmpTouchedPlayerCooties);
-        removeCooties(touchPlayer);
-
+    public List<CootiesContext> getCooties(){
+        return cooties;
     }
 
-    private static void removeCooties(Player p){
-        //if (cooties.get(p.getName()).contains(hogehoge){
-        //    cooties.get(p.getName()).remove(hogehoge);
-        //}
+    public Player getPlayer(){
+        return player;
+    }
+
+    @Override
+    public PlayerState clone(){
+        PlayerState ps = null;
+        try {
+            ps = (PlayerState)super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return ps;
     }
 }
