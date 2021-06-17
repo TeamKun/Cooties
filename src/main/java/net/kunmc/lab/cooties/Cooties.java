c133fe93312dc1dbec475d52e9d3734b0978a957
POne0301 <onexttt@gmail.com>
2021-06-18 02:19:19 +0900

    add BukkitTask to Cooties

--- src/main/java/net/kunmc/lab/cooties/Cooties.java
+++ src/main/java/net/kunmc/lab/cooties/Cooties.java
@@ -1,13 +1,16 @@
 package net.kunmc.lab.cooties;
 
+import net.kunmc.lab.cooties.task.Task;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 
 public final class Cooties extends JavaPlugin {
+    private BukkitTask task;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        task = new Task(this).runTaskTimer(this, 0, 1);
     }
 
     @Override
