1aa061f5b25b6b47caa997c726b67f06b4a5277e
POne0301 <onexttt@gmail.com>
2021-06-19 01:36:14 +0900

    cootiesをとりあえず揃えておく

--- src/main/java/net/kunmc/lab/cooties/cooties/players/ConfusionCooties.java
+++ src/main/java/net/kunmc/lab/cooties/cooties/players/ConfusionCooties.java
@@ -1,7 +1,9 @@
 package net.kunmc.lab.cooties.cooties.players;
 
+import net.kunmc.lab.cooties.Config;
 import net.kunmc.lab.cooties.cooties.CootiesInterface;
 import net.kunmc.lab.cooties.cooties.CootiesState;
+import org.bukkit.Particle;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -13,8 +15,12 @@ public class ConfusionCooties extends CootiesState implements CootiesInterface {
 
     @Override
     public void runCootiesProcess(Player p) {
+        p.getLocation().getWorld().spawnParticle(Particle.DRAGON_BREATH, p.getLocation(), 1);
+        if (p.getName().equals(Config.confusionCootiesPlayerName))
+            return;
+
         int time = getTime();
-        if (getTime() == 0) {
+        if (time == 0) {
             // TODO: 10はConfigで設定するか検討
             p.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 20 * 10, 0, false, false));
         }
